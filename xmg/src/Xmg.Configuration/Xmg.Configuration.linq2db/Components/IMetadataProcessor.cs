2eac4e2dc85d699d7bf73a7eec461c2c5578f671
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-05-07 19:22:12 +0300

    xmg: add linq2db metadata processor

---
+++ xmg/src/Xmg.Configuration/Xmg.Configuration.linq2db/Components/IMetadataProcessor.cs
@@ -0,0 +1,10 @@
+using Xmg.Core.Models;
+using LDatabase = Annium.linq2db.Extensions.Models.Database;
+
+namespace Xmg.Configuration.linq2db.Components
+{
+    internal interface IMetadataProcessor
+    {
+        Database Process(LDatabase database);
+    }
+}
