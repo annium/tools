8d81bb71734277b399b068d1c0b896322fe0386e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-05-10 23:45:04 +0300

    xmg: add debugger...

---
+++ xmg/src/Debugger/CustomLoadContext.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.DependencyModel.Resolution;
+
+namespace Debugger
+{
+    public class CustomLoadContext : AssemblyLoadContext
+    {
+        private readonly AssemblyDependencyResolver _builtInResolver;
+        private readonly CustomAssemblyResolver _customResolver;
+        private readonly CustomAssemblyResolver _customDefaultContextResolver;
+
+        public CustomLoadContext(string lambdaPath)
+            : base("CustomLoadContext")
+        {
+            _builtInResolver = new AssemblyDependencyResolver(lambdaPath);
+            _customResolver = new CustomAssemblyResolver(this, lambdaPath);
+
+            _customDefaultContextResolver = new CustomAssemblyResolver(Default, lambdaPath);
+
+            Default.Resolving += OnDefaultAssemblyLoadContextResolving;
+        }
+
+        private Assembly OnDefaultAssemblyLoadContextResolving(AssemblyLoadContext context, AssemblyName assemblyName)
+        {
+            string assemblyPath = _customDefaultContextResolver.ResolveAssemblyToPath(assemblyName);
+
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            if (assemblyName.Name.StartsWith("Amazon.Lambda.Core"))
+                return null;
+
+            var assemblyPath = _builtInResolver.ResolveAssemblyToPath(assemblyName);
+            if (assemblyPath == null || !File.Exists(assemblyPath))
+            {
+                assemblyPath = _customResolver.ResolveAssemblyToPath(assemblyName);
+            }
+
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            var libraryPath = _builtInResolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+            if (libraryPath != null)
+            {
+                return LoadUnmanagedDllFromPath(libraryPath);
+            }
+
+            return IntPtr.Zero;
+        }
+
+
+        private class CustomAssemblyResolver
+        {
+            private readonly ICompilationAssemblyResolver _assemblyResolver;
+            private readonly DependencyContext _dependencyContext;
+
+            public CustomAssemblyResolver(
+                AssemblyLoadContext assemblyLoadContext,
+                string rootAssemblyPath
+            )
+            {
+                var assembly = assemblyLoadContext.LoadFromAssemblyPath(rootAssemblyPath);
+                _dependencyContext = DependencyContext.Load(assembly);
+
+                _assemblyResolver = new CompositeCompilationAssemblyResolver
+                (new ICompilationAssemblyResolver[]
+                {
+                    new AppBaseCompilationAssemblyResolver(Path.GetDirectoryName(rootAssemblyPath)),
+                    new ReferenceAssemblyPathResolver(),
+                    new PackageCompilationAssemblyResolver()
+                });
+            }
+
+            public string ResolveAssemblyToPath(AssemblyName name)
+            {
+                bool NamesMatch(RuntimeLibrary runtime)
+                {
+                    return string.Equals(runtime.Name, name.Name, StringComparison.OrdinalIgnoreCase);
+                }
+
+                bool ResourceAssetPathMatch(RuntimeLibrary runtime)
+                {
+                    foreach (var group in runtime.RuntimeAssemblyGroups)
+                    {
+                        foreach (var path in group.AssetPaths)
+                        {
+                            if (path.EndsWith("/" + name.Name + ".dll"))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+
+                    return false;
+                }
+
+                RuntimeLibrary library =
+                    this._dependencyContext.RuntimeLibraries.FirstOrDefault(NamesMatch);
+
+                if (library == null)
+                    library = this._dependencyContext.RuntimeLibraries.FirstOrDefault(ResourceAssetPathMatch);
+
+                if (library != null)
+                {
+                    var wrapper = new CompilationLibrary(
+                        library.Type,
+                        library.Name,
+                        library.Version,
+                        library.Hash,
+                        library.RuntimeAssemblyGroups.SelectMany(g => g.AssetPaths),
+                        library.Dependencies,
+                        library.Serviceable);
+
+                    var assemblies = new List<string>();
+                    this._assemblyResolver.TryResolveAssemblyPaths(wrapper, assemblies);
+                    if (assemblies.Count > 0)
+                    {
+                        return assemblies[0];
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ xmg/src/Debugger/Program.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Annium.linq2db.Extensions;
+
+namespace Debugger
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var path = "/projects/annium/id/server/src/Shared/Annium.Id.Db/bin/Debug/netcoreapp3.1/Annium.Id.Db.dll";
+
+            var lc = new CustomLoadContext(path);
+            var assembly = lc.LoadFromAssemblyPath(path);
+            var types = assembly.GetTypes();
+
+            var cfgType = typeof(IEntityConfiguration<>);
+            var cfgTypeName = cfgType.AssemblyQualifiedName;
+
+            var exact = CollectTypes(assembly, x => x == cfgType);
+            var named = CollectTypes(assembly, x => x.AssemblyQualifiedName == cfgTypeName);
+            var namedCfgType = named.First().configurationType.GetInterfaces().First().GetGenericTypeDefinition();
+
+            Console.WriteLine("Done");
+        }
+
+        private static (Type configurationType, Type entityType)[] CollectTypes(Assembly assembly, Predicate<Type> predicate)
+        {
+            var allTypes = assembly.GetTypes();
+            var concreteClasses = allTypes.Where(x => x.IsClass && !x.IsAbstract && !x.IsGenericType).ToArray();
+
+            var configurationTypes = concreteClasses
+                .Select(x => (
+                    x,
+                    i: x.GetInterfaces().SingleOrDefault(i => i.IsGenericType && predicate(i.GetGenericTypeDefinition()))
+                ))
+                .Where(p => p.i != null)
+                .Select(p => (p.x, p.i.GenericTypeArguments.Single()))
+                .ToArray();
+
+            return configurationTypes;
+        }
+    }
+
+    internal class PluginLoadContext : AssemblyLoadContext
+    {
+        private readonly AssemblyDependencyResolver _resolver;
+
+        public PluginLoadContext(string assemblyPath)
+        {
+            _resolver = new AssemblyDependencyResolver(assemblyPath);
+        }
+
+        protected override Assembly? Load(AssemblyName assemblyName)
+        {
+            var assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+
+            return assemblyPath is null ? null : LoadFromAssemblyPath(assemblyPath);
+        }
+    }
+}
