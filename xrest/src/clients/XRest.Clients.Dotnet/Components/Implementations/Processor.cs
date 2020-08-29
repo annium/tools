99f00f16cc0b87d23ef0e9aaae6304a514db9956
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-08-29 20:20:16 +0300

    xrest: improve flow to call methods with default and handle results specially

--- xrest/src/clients/XRest.Clients.Dotnet/Components/Implementations/Processor.cs
+++ xrest/src/clients/XRest.Clients.Dotnet/Components/Implementations/Processor.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Annium.Core.Reflection;
+using Annium.Core.Runtime.Types;
+using Annium.Data.Operations;
 using Annium.Extensions.Primitives;
 using XRest.Clients.Dotnet.Views;
 using XRest.Core.Models;
@@ -17,9 +20,10 @@ namespace XRest.Clients.Dotnet.Components.Implementations
 
         public ClientContainerView Process(string ns, ApiModel api)
         {
+            var tm = TypeManager.GetInstance(api.Assembly);
             var clients = api.Controllers
-                .GroupBy(x => (x.Area! ?? string.Empty).PascalCase())
-                .ToDictionary(x => x.Key, x => x.Select(xx => ParseController(ns, xx)).ToArray());
+                .GroupBy(x => (x.Area ?? string.Empty).PascalCase())
+                .ToDictionary(x => x.Key, x => x.Select(xx => ParseController(ns, xx, tm)).ToArray());
 
             var containers = clients
                 .Where(x => !string.IsNullOrWhiteSpace(x.Key))
@@ -39,10 +43,10 @@ namespace XRest.Clients.Dotnet.Components.Implementations
             return new ClientContainerView(usages, ns, name, type, clients);
         }
 
-        private ClientView ParseController(string projectName, ControllerModel controller)
+        private ClientView ParseController(string projectName, ControllerModel controller, ITypeManager tm)
         {
             var references = controller.Actions
-                .SelectMany(CollectReferences)
+                .SelectMany(x => CollectReferences(x, tm))
                 .Distinct()
                 .Where(x => !IsBaseType(x))
                 .Select(x => x.Namespace!)
@@ -53,7 +57,7 @@ namespace XRest.Clients.Dotnet.Components.Implementations
                 ? BuildNamespace(projectName, Clients)
                 : BuildNamespace(projectName, Areas, controller.Area.PascalCase(), Clients);
             var usages = BuildUsages(ns, references);
-            var actions = controller.Actions.Select(ParseAction).ToArray();
+            var actions = controller.Actions.Select(x => ParseAction(x, tm)).ToArray();
 
             return new ClientView(
                 usages,
@@ -64,33 +68,40 @@ namespace XRest.Clients.Dotnet.Components.Implementations
             );
         }
 
-        private IReadOnlyCollection<Type> CollectReferences(ActionModel action)
+        private IReadOnlyCollection<Type> CollectReferences(ActionModel action, ITypeManager tm)
         {
             var references = new HashSet<Type>();
 
             foreach (var parameter in action.Parameters)
-                CollectReferences(parameter.Type);
+                CollectTypeReferences(parameter.Type);
 
             if (action.Body != null)
-                CollectReferences(action.Body);
+                CollectTypeReferences(action.Body);
 
             if (action.Response != null)
-                CollectReferences(action.Response);
+            {
+                CollectTypeReferences(action.Response);
+                var response = ResolveResponseType(action.Response);
+                var (_, responseType) = ResolveResponseKindAndInnerType(response);
+                var defaultType = ResolveTypeDefaultType(responseType, tm);
+                if (defaultType != null)
+                    CollectTypeReferences(action.Response);
+            }
 
             return references;
 
-            void CollectReferences(Type type)
+            void CollectTypeReferences(Type type)
             {
                 references.Add(type);
 
                 if (type.IsGenericType)
                     foreach (var argument in type.GetGenericArguments())
-                        CollectReferences(argument);
+                        CollectTypeReferences(argument);
             }
         }
 
 
-        private ActionView ParseAction(ActionModel action)
+        private ActionView ParseAction(ActionModel action, ITypeManager tm)
         {
             var pathParameters = action.Parameters
                 .Where(x => x.Location == ParameterLocationEnum.Path)
@@ -102,26 +113,127 @@ namespace XRest.Clients.Dotnet.Components.Implementations
                 .Select(x => new ParameterView(x.Name, x.Type.FriendlyName()))
                 .ToArray();
 
+            var response = ResolveResponseType(action.Response);
+            var (kind, responseType) = ResolveResponseKindAndInnerType(response);
+            var defaultType = ResolveTypeDefaultType(responseType, tm);
+            var responseDefault = GetDefaultExpression(kind, responseType, defaultType);
+
             return new ActionView(
                 action.Name,
                 action.Method,
                 action.Path,
                 pathParameters,
                 queryParameters,
-                action.Body?.FriendlyName(),
-                ResolveResponseType(action.Response)?.FriendlyName()
+                action.Body?.FriendlyName() ?? string.Empty,
+                response?.FriendlyName() ?? string.Empty,
+                responseDefault
             );
+        }
 
-            static Type? ResolveResponseType(Type? type)
-            {
-                if (type is null || type == typeof(Task))
-                    return null;
+        private Type? ResolveResponseType(Type? type)
+        {
+            if (type is null || type == typeof(Task))
+                return null;
 
-                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Task<>))
-                    return type.GetGenericArguments()[0];
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Task<>))
+                return type.GetGenericArguments()[0];
 
+            return type;
+        }
+
+        private (ResponseKind, Type?) ResolveResponseKindAndInnerType(Type? type)
+        {
+            if (type is null)
+                return (ResponseKind.None, null);
+
+            if (type.FullName == typeof(IResult).FullName)
+                return (ResponseKind.Result, null);
+
+            var isGenericResult = type.IsGenericType && type.GetGenericTypeDefinition().FullName == typeof(IResult<>).FullName;
+            if (isGenericResult)
+                return (ResponseKind.DataResult, type.GetGenericArguments()[0]);
+
+            return (ResponseKind.Plain, type);
+        }
+
+        /// <summary>
+        /// Returns type, assignable to given type, that can be used as default value (is struct or class with parameterless constructor)
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="tm"></param>
+        /// <returns></returns>
+        private Type? ResolveTypeDefaultType(Type? type, ITypeManager tm)
+        {
+            if (type is null)
+                return null;
+
+            if (type.IsValueType)
                 return type;
+
+            if (type.IsClass)
+            {
+                var classDefault = !type.IsAbstract && type.HasDefaultConstructor()
+                    ? type
+                    : tm.GetImplementations(type)
+                        .FirstOrDefault(x => !x.IsAbstract && x.HasDefaultConstructor());
+
+                return classDefault;
             }
+
+            // special handling for IEnumerable<> and dictionaries
+            var enumerableImplementation = type.GetTargetImplementation(typeof(IEnumerable<>));
+            if (enumerableImplementation != null)
+            {
+                var elementType = enumerableImplementation.GetGenericArguments()[0];
+                var keyValueImplementation = elementType.GetTargetImplementation(typeof(KeyValuePair<,>));
+
+                var enumerableDefault = keyValueImplementation is null
+                    ? elementType.MakeArrayType()
+                    : typeof(Dictionary<,>).MakeGenericType(keyValueImplementation.GetGenericArguments());
+
+                return enumerableDefault;
+            }
+
+            // special handling for IEnumerable and friends
+            if (type.IsAssignableFrom(typeof(Array)))
+                return typeof(Array);
+
+            var defaultType = tm.GetImplementations(type)
+                .FirstOrDefault(x => x.IsValueType || x.IsClass && !x.IsAbstract && x.HasDefaultConstructor());
+
+            return defaultType;
+        }
+
+        private string GetDefaultExpression(ResponseKind kind, Type? responseType, Type? defaultType) => kind switch
+        {
+            ResponseKind.Plain  => GetDefaultExpression(defaultType),
+            ResponseKind.Result => @"Result.New().Error(""Request failed"")",
+            ResponseKind.DataResult => responseType!.FullName == defaultType!.FullName
+                ? $@"Result.New({GetDefaultExpression(defaultType)}).Error(""Request failed"")"
+                : $@"Result.New<{responseType.FriendlyName()}>({GetDefaultExpression(defaultType)}).Error(""Request failed"")",
+            _ => string.Empty,
+        };
+
+        private string GetDefaultExpression(Type? type)
+        {
+            if (type is null)
+                return string.Empty;
+
+            if (type.IsValueType)
+                return $"default({type.FriendlyName()})";
+
+            if (type.IsArray)
+                return $"Array.Empty<{type.GetElementType()!.FriendlyName()}>()";
+
+            return $"new {type.FriendlyName()}()";
+        }
+
+        private enum ResponseKind
+        {
+            None,
+            Plain,
+            Result,
+            DataResult,
         }
     }
 }
--- xrest/src/clients/XRest.Clients.Dotnet/Views/ActionView.cs
+++ xrest/src/clients/XRest.Clients.Dotnet/Views/ActionView.cs
@@ -14,9 +14,10 @@ namespace XRest.Clients.Dotnet.Views
         public IReadOnlyCollection<ParameterView> PathParameters { get; }
         public IReadOnlyCollection<ParameterView> QueryParameters { get; }
         public bool HasBody { get; }
-        public string? Body { get; }
+        public string Body { get; }
         public bool HasResponse { get; }
-        public string? Response { get; }
+        public string Response { get; }
+        public string ResponseDefault { get; }
 
         public ActionView(
             string name,
@@ -24,8 +25,9 @@ namespace XRest.Clients.Dotnet.Views
             string path,
             IReadOnlyCollection<ParameterView> pathParameters,
             IReadOnlyCollection<ParameterView> queryParameters,
-            string? body,
-            string? response
+            string body,
+            string response,
+            string responseDefault
         )
         {
             Name = name;
@@ -37,6 +39,7 @@ namespace XRest.Clients.Dotnet.Views
             Body = body;
             HasResponse = !string.IsNullOrWhiteSpace(response);
             Response = response;
+            ResponseDefault = responseDefault;
             if (HasBody)
                 Parameters = pathParameters.Concat(queryParameters).Append(new ParameterView("body", Body!)).ToArray();
             else
