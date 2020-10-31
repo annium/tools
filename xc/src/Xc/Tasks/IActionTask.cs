281ba16b00b90f0a91620106e60c1a56cefb42c5
Alex Kreskiyan <a.kreskiyan@gmail.com>
2020-10-31 17:47:59 +0300

    xc: define tasks shape

---
+++ xc/src/Xc/Tasks/IActionTask.cs
@@ -0,0 +1,22 @@
+namespace Xc.Tasks
+{
+    internal interface IActionTask : ITask
+    {
+        void Execute();
+    }
+
+    internal interface IActionTask<T> : ITask
+    {
+        void Execute(T x1);
+    }
+
+    internal interface IActionTask<T1, T2> : ITask
+    {
+        void Execute(T1 x1, T2 x2);
+    }
+
+    internal interface IActionTask<T1, T2, T3> : ITask
+    {
+        void Execute(T1 x1, T2 x2, T3 x3);
+    }
+}
--- xc/src/Xc/Tasks/ITask.cs
+++ xc/src/Xc/Tasks/ITask.cs
@@ -1,14 +1,7 @@
-using System.Threading.Tasks;
-
 namespace Xc.Tasks
 {
     internal interface ITask
     {
-        void Execute();
-    }
 
-    internal interface ITask<T>
-    {
-        T Execute();
     }
 }
