df632c2129a7b951f35da106b417b314fd65499e
Alex Kreskiyan <a.kreskiyan@gmail.com>
2021-06-12 21:01:48 +0300

    xsass: update

--- xsass/src/XSass/ServicePack.cs
+++ xsass/src/XSass/ServicePack.cs
@@ -1,7 +1,7 @@
 using System;
 using Annium.Configuration.Abstractions;
 using Annium.Core.DependencyInjection;
-using Annium.Logging.Abstractions;
+using Annium.Logging.Shared;
 using XSass.Internal;
 using XSass.Internal.Components;
 
