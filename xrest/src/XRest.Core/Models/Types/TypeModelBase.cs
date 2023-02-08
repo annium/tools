570a1023f3eda728701da9fefe1ac0e8a0e2895c
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-02-08 18:06:45 +0300

    xrest: cleanup TypeModelBase

--- xrest/src/XRest.Core/Models/Types/TypeModelBase.cs
+++ xrest/src/XRest.Core/Models/Types/TypeModelBase.cs
@@ -1,5 +1,3 @@
 namespace XRest.Core.Models.Types;
 
-public abstract record TypeModelBase(Namespace Namespace, string Name, bool IsGeneric) : ITypeModel
-{
-}
+public abstract record TypeModelBase(Namespace Namespace, string Name, bool IsGeneric) : ITypeModel;
