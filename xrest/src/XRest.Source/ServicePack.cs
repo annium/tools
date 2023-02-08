e38ddca8b26889834560cc6496b240c93a46c106
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-02-08 17:56:19 +0300

    xrest: update source service pack

--- xrest/src/XRest.Source/ServicePack.cs
+++ xrest/src/XRest.Source/ServicePack.cs
@@ -1,5 +1,6 @@
 using System;
 using Annium.Core.DependencyInjection;
+using XRest.Core;
 using XRest.Source.Components;
 using XRest.Source.Internal.Components;
 
@@ -10,6 +11,7 @@ public class ServicePack : ServicePackBase
     public override void Register(IServiceContainer container, IServiceProvider provider)
     {
         container.AddHttpRequestFactory().SetDefault();
+        container.AddXRestSerializer();
         container.Add<ILoader, Loader>().Singleton();
     }
 }
