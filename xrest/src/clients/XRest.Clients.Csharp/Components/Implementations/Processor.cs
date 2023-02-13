a61abdaa5848ab1dc497bfe8738625a41fe4d8da
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-02-14 00:03:59 +0300

    xrest: cs init new processor

---
+++ xrest/src/clients/XRest.Clients.Csharp/Components/Implementations/Processor.cs
@@ -0,0 +1,13 @@
+using Annium.Net.Types.Models;
+using XRest.Clients.Csharp.Views;
+using XRest.Core.Models;
+
+namespace XRest.Clients.Csharp.Components.Implementations;
+
+internal class Processor : IProcessor
+{
+    public ClientContainerView Process(Namespace ns, ApiModel api)
+    {
+        throw new System.NotImplementedException();
+    }
+}
