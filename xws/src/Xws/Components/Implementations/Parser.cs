04f154dfdad443f3fde0d3987ce3a77bb19a700d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-10-22 17:47:50 +0300

    xws: init update for mesh

--- xws/src/Xws/Components/Implementations/Parser.cs
+++ xws/src/Xws/Components/Implementations/Parser.cs
@@ -20,10 +20,10 @@ internal class Parser : IParser
     public ApiModel Parse(Assembly assembly, string name, ITypeManager tm)
     {
         var broadcasters = GetRawModels(tm, typeof(IBroadcaster<>)).Select(ParseBroadcaster).ToArray();
-        var eventHandlers = GetRawModels(tm, typeof(IEventHandler<,>)).Select(ParseEventHandler).ToArray();
+        var eventHandlers = GetRawModels(tm, typeof(IEventHandler<>)).Select(ParseEventHandler).ToArray();
         var requestHandlers = GetRawModels(tm, typeof(IRequestHandler<,>)).Select(ParseRequestHandler).ToArray();
         var requestResponseHandlers = GetRawModels(tm, typeof(IRequestResponseHandler<,,>)).Select(ParseRequestResponseHandler).ToArray();
-        var subscriptionHandlers = GetRawModels(tm, typeof(ISubscriptionHandler<,,>)).Select(ParseSubscriptionHandler).ToArray();
+        var subscriptionHandlers = GetRawModels(tm, typeof(ISubscriptionHandler<,>)).Select(ParseSubscriptionHandler).ToArray();
 
         return new ApiModel(
             assembly,
