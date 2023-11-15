cdee5f9157b5b9a5151f2b179aaf58e080a26c48
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-11-15 09:49:48 +0300

    .NET 8

--- xdb/src/Xdb.Core/Migrations/MigrationEngineBase.cs
+++ xdb/src/Xdb.Core/Migrations/MigrationEngineBase.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
-using Annium.Linq;
 using DbUp.Builder;
 
 namespace Xdb.Core.Migrations;
