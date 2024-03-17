9241199dd3a392d20c4454079106e7f9c7035c24
Alex Kreskiyan <a.kreskiyan@gmail.com>
2024-03-17 17:28:01 +0300

    format

--- xdb/src/Xdb.PostgreSql/MigratorExtensions.cs
+++ xdb/src/Xdb.PostgreSql/MigratorExtensions.cs
@@ -6,11 +6,7 @@ namespace Xdb;
 
 public static class MigratorExtensions
 {
-    public static PostgresqlMigrationEngine ForPostgresql(
-        this Migrator _,
-        string connectionString,
-        string schema
-    ) =>
+    public static PostgresqlMigrationEngine ForPostgresql(this Migrator _, string connectionString, string schema) =>
         new(
             DeployChanges.To.PostgresqlDatabase(connectionString),
             DeployChanges.To.PostgresqlDatabase(connectionString),
